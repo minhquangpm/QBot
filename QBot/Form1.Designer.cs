98bf25464d06fea6c747bc9cac8d399ce24b736a
minhquangpm <deluminous01@gmail.com>
2018-10-04 01:41:47 +0700

    quangpm
    change some text

--- QBot/Form1.Designer.cs
+++ QBot/Form1.Designer.cs
@@ -183,7 +183,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "QBot Maple M v0.2";
+            this.Text = "QBot v0.2";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.form1_FormClosed);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
